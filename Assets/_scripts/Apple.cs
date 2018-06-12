23c7374ead646ebd4165e715b7fae9b5ca4add91
JFU5 <JFU5@CILABS-114B-10.scilsnet.rutgers.edu>
2018-06-12 14:58:00 -0400

    Added scoring system
    Added scripts to increment score counter on apple collection

--- Assets/_scripts/Apple.cs
+++ Assets/_scripts/Apple.cs
@@ -9,6 +9,9 @@ public class Apple : MonoBehaviour {
 	void Update () {
 		if (transform.position.y < bottomY) {
             Destroy(this.gameObject);
+
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
         }
 	}
 }
